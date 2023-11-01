74ec0dec4882e69255838304fb9df1dd84cc90f5
Fatin Noor <120398849+fatin2030@users.noreply.github.com>
2023-11-01 16:42:52 +0600

    logout.php

--- LAB_EXAM_SET_A/controller/logout.php
+++ LAB_EXAM_SET_A/controller/logout.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-$_SESSION = array();
-session_destroy();
-header("Location: ../view/login.php");
-exit;
+    session_destroy();
+   // unlink($_SESSION['flag']);
+    setcookie('flag', 'true', time()-10, '../view/login.html');
+    header('location: ../view/login.html');
 ?>
